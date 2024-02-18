613cdc44c4f9117a1a391d68ae345e5a176c800a
Brady Petterson <Brady.Petterson@gmail.com>
2024-02-17 20:27:15 -0600

    Add project files.

---
+++ LearnMyCalculatorAppTests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnMyCalculatorApp;
+
+namespace LearnMyCalculatorApp.Test {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void CalculatorNullTest () {
+			var calculator = new Calculator();
+			Assert.IsNotNull(calculator);
+		}
+		[TestMethod]
+		public void AddTest () {
+			// Arrange
+			var calculator = new Calculator();
+
+			// Act
+			var actual = calculator.Add(1, 1);
+
+			// Assert
+			Assert.AreEqual(2, actual);
+		}
+		[TestMethod]
+		public void SubtractTest () {
+			// Arrange
+			var calculator = new Calculator();
+
+			// Act
+			var actual = calculator.Subtract(1, 1);
+
+			// Assert
+			Assert.AreEqual(0, actual);
+		}
+	}
+}
